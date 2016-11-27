e045d71fef32d8de6196ae4ee60bf7badef740ec
Xinchen Yan <xcyan@umich.edu>
2016-11-27 15:44:18 -0800

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,4 +1,7 @@
 mkdir ./data
+wget -O data/all_ids.tar.gz https://umich.box.com/s/4m1mr6aud793gwi7jn266t32w83ml25l
+wget -O data/all_viewdata.tar.gz https://umich.box.com/s/ckvihxh4berjzcgd3s8aiu87aurv3gms
+wget -O data/all_voxdata.tar.gz https://umich.box.com/s/bwyx8qsby2f38ju1ybcrp50a1q9uzenu
 tar xf data/all_ids.tar.gz -C data/
 tar xf data/all_viewdata.tar.gz -C data/
 tar xf data/all_voxdata.tar.gz -C data/
