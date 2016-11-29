2e2fa4d4cfdf749b7d3ab31c2eabe5ee94885caf
xcyan <xcyan@umich.edu>
2016-11-29 01:01:49 -0500

    fix bugs based on yijie's comment

--- prepare_data.sh
+++ prepare_data.sh
@@ -3,5 +3,6 @@ wget -O data/all_ids.tar.gz https://umich.box.com/shared/static/4m1mr6aud793gwi7
 wget -O data/all_viewdata.tar.gz https://umich.box.com/shared/static/ckvihxh4berjzcgd3s8aiu87aurv3gms.gz
 wget -O data/all_voxdata.tar.gz https://umich.box.com/shared/static/bwyx8qsby2f38ju1ybcrp50a1q9uzenu.gz
 tar xf data/all_ids.tar.gz -C data/
+echo "It may take a while, please be patient."
 tar xf data/all_viewdata.tar.gz -C data/
 tar xf data/all_voxdata.tar.gz -C data/
