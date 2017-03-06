f07d5ada9d81928d17663b4f05229154ac087a85
Xinchen Yan <xcyan@umich.edu>
2017-03-05 16:56:43 -0800

    Create download_models.sh

---
+++ download_models.sh
@@ -0,0 +1,4 @@
+mkdir ./models
+wget -O models/deeprotator_rnn16.t7 https://umich.box.com/shared/static/6mci62a2f7qri1oqe1idzr7qvrmvltsg.t7
+wget -O models/ptn_comb.t7 https://umich.box.com/shared/static/0f3mtxxrjtl5d264e9g69k6b9otfj9od.t7
+wget -O models/cnn_vol.t7 https://umich.box.com/shared/static/dfy6yqx4v9a5cq97left17iorr14arjo.t7
