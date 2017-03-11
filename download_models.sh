cca317b21f3a363cdb52235e2f4c9dda47814f53
Xinchen Yan <xcyan@umich.edu>
2017-03-10 16:19:55 -0800

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,4 @@
 mkdir ./models
-wget -O models/deeprotator_rnn16.t7 https://umich.box.com/shared/static/6mci62a2f7qri1oqe1idzr7qvrmvltsg.t7
-wget -O models/ptn_proj.t7 https://umich.box.com/shared/static/x8z48x98cqc38plqqp2n9cef1s6k01o0.t7
-wget -O models/ptn_comb.t7 https://umich.box.com/shared/static/0f3mtxxrjtl5d264e9g69k6b9otfj9od.t7
-wget -O models/cnn_vol.t7 https://umich.box.com/shared/static/dfy6yqx4v9a5cq97left17iorr14arjo.t7
+wget -O models/ptn_proj.t7 
+wget -O models/ptn_comb.t7 
+wget -O models/cnn_vol.t7 
