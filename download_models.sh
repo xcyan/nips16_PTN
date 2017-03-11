ce98265c916aba340ff235667e0b3b2ed63cf05a
xcyan <xcyan@umich.edu>
2017-03-10 20:18:52 -0500

    fixed one bug in download_models

--- download_models.sh
+++ download_models.sh
@@ -1,4 +1,5 @@
 mkdir ./models
-wget -O models/ptn_proj.t7 
-wget -O models/ptn_comb.t7 
-wget -O models/cnn_vol.t7 
+wget -O models/ptn_proj.t7 https://umich.box.com/shared/static/5z5oci8lwdhzszf88s5azbeq1nsa15ad.t7
+wget -O models/ptn_comb.t7 https://umich.box.com/shared/static/t901uwklqy4a4d0woad9znvdlkrzh1zk.t7 
+wget -O models/cnn_vol.t7 https://umich.box.com/shared/static/2ur48aj419slj8cwkeobubhwnvripc0s.t7
+
